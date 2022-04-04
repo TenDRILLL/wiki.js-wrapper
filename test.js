190119e86af8ac6c364e3ddc29be2725df0d7a33
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-04-04 15:50:24 +0300

    implement a test case for search

--- test.js
+++ test.js
@@ -6,6 +6,13 @@ client.login().then((title)=>{
     console.log(`Connected to ${title}`);
     client.pages.get(1).then(page => {
         console.log(page);
+    }).catch(e => {
+        console.log(e);
+    });
+    client.pages.search("cmd").then(searchResult => {
+        console.log(`Amount of pages matching your search: ${searchResult.totalHits}`);
+    }).catch(e => {
+        console.log(e);
     });
 }).catch(e => {
     console.log(e);
