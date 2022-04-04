8f76562db88dc57c29fd27ad3e1a372027371369
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-04-04 15:33:36 +0300

    implement a test for get function

--- test.js
+++ test.js
@@ -4,6 +4,9 @@ const client = new Client(options);
 
 client.login().then((title)=>{
     console.log(`Connected to ${title}`);
+    client.pages.get(1).then(page => {
+        console.log(page);
+    });
 }).catch(e => {
     console.log(e);
 });
