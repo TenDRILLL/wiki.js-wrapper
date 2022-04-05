c57701b0ad6dae09cb25e6e39aba1c420c97df94
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-04-05 04:52:31 +0300

    reject with error if client not ready

--- src/PageManager.js
+++ src/PageManager.js
@@ -5,7 +5,7 @@ class PageManager{
 
     get(id){
         return new Promise((resolve,reject)=> {
-            if(!this.client.isReady()) reject("CLIENT_NOT_READY");
+            if(!this.client.isReady()) reject(new Error("CLIENT_NOT_READY"));
             this.client.APIRequest.req(`?query={pages{single(id:${id}){id,path,hash,title,description,isPrivate,isPublished,privateNS,publishStartDate,publishEndDate,tags{id,tag,title,createdAt,updatedAt},content,render,contentType,createdAt,updatedAt,editor,locale,scriptCss,scriptJs,authorId,authorName,authorEmail,creatorId,creatorName,creatorEmail}}}`).then(data => {
                 if(data.data.pages.single.id){
                     resolve(data.data.pages.single);
@@ -20,7 +20,7 @@ class PageManager{
 
     search(searchQuery, path, locale){
         return new Promise((resolve, reject)=>{
-            if(!this.client.isReady()) reject("CLIENT_NOT_READY");
+            if(!this.client.isReady()) reject(new Error("CLIENT_NOT_READY"));
             this.client.APIRequest.req(`?query={pages{search(query:"${searchQuery}"${path ? `,path:"${path}"` : ""}${locale ? `,locale:"${locale}"` : ""}){results{id,title,description,path,locale},suggestions,totalHits}}}`).then(data => {
                 if(data.data.pages.search){
                     resolve(data.data.pages.search);
