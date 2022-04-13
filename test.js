3bde38d2a5ab34972412f3ba18cfb9b7bb5092b4
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-04-13 20:33:18 +0300

    Invalid token test throws expected error but doesn't pass

--- test.js
+++ test.js
@@ -1,18 +1,5 @@
 const nock = require("nock");
 
-const login = nock("https://example.com", {
-        reqheaders: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOjEsImdycCI6MSwiaWF0IjoxNjQ5MjAzMjAwLCJleHAiOjE2ODA3MzkyMDAsImF1ZCI6InVybjp3aWtpLmpzIiwiaXNzIjoidXJuOndpa2kuanMiLCJ0eXBlIjoiZmFrZS1hc3MtdG9rZW4ifQ.FEWmrlsNrmbf9ESIgOhECNB_N9wRofUbM6UYLGpUrlw"
-        }
-    })
-    .get("/graphql?query={site{config{title}}}")
-    .reply(200,{data: {site: {config: {title: "Example"}}}})
-    .persist();
-
-const redirect = nock("http://example.com")
-    .get("/graphql?query={site{config{title}}}")
-    .reply(301,{},{location: "https://example.com/graphql?query={site{config{title}}}"});
-
 describe("Login to the API.", ()=>{
     const Client = require("./src/Client");
     let client;
@@ -22,21 +9,32 @@ describe("Login to the API.", ()=>{
     }
 
     it("Returns Wiki.js website title.", async ()=>{
+        const login = nock("https://example.com")
+            .get("/graphql?query={site{config{title}}}")
+            .reply(200,{data: {site: {config: {title: "Example"}}}});
         client = new Client(testOptions);
         expect(await client.login()).toBe("Example");
     });
 
     it("Returns Wiki.js website title after HTTP redirect.", async ()=>{
+        const login = nock("https://example.com")
+            .get("/graphql?query={site{config{title}}}")
+            .reply(200,{data: {site: {config: {title: "Example"}}}});
+        const redirect = nock("http://example.com")
+            .get("/graphql?query={site{config{title}}}")
+            .reply(301,{},{location: "https://example.com/graphql?query={site{config{title}}}"});
         testOptions.baseURL = "http://example.com/graphql";
         client = new Client(testOptions);
         expect(await client.login()).toBe("Example");
     });
 
-    //TODO: FIX: ERR_NOCK_NO_MATCH, if reqheaders are invalid nock doesn't return at all.
     it("Fail returning Wiki.js title if token is invalid.", async ()=>{
+        const fail = nock("https://example.com")
+            .get("/graphql?query={site{config{title}}}")
+            .reply(200,{errors: [{message: "Forbidden"}]});
         testOptions.token = "ThisIsAFakeToken";
         testOptions.baseURL = "https://example.com/graphql";
         client = new Client(testOptions);
-        expect(await client.login()).toThrowError("Error: Forbidden");
+        expect(await client.login()).toThrow("Error: Forbidden"); //TODO: FIX, right now only throws Error: Forbidden.
     });
 });
